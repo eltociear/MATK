b7fb8a8c3d8c29dc43cfd40db43cf79345736702
Nguyen Hoang <hoangkhoinguyen2002@gmail.com>
2023-03-28 03:45:35 +0000

    add exception handling

--- preprocessing/inpainting/inpainting.py
+++ preprocessing/inpainting/inpainting.py
@@ -13,36 +13,39 @@ def midpoint(x1, y1, x2, y2):
 
 def inpaint_text(img_path):
     # read the image 
-    img = keras_ocr.tools.read(img_path) 
-    
-    # Recogize text (and corresponding regions)
-    # Each list of predictions in prediction_groups is a list of
-    # (word, box) tuples. 
-    pipeline = keras_ocr.pipeline.Pipeline()
-    prediction_groups = pipeline.recognize([img])
-    #Define the mask for inpainting
-    mask = np.zeros(img.shape[:2], dtype="uint8")
-    rec = 0
-    for box in prediction_groups[0]:
-        rec = 1 #in case pipeline not recognize
-        x0, y0 = box[1][0]
-        x1, y1 = box[1][1] 
-        x2, y2 = box[1][2]
-        x3, y3 = box[1][3] 
+    try:
+        img = keras_ocr.tools.read(img_path) 
+        # Recogize text (and corresponding regions)
+        # Each list of predictions in prediction_groups is a list of
+        # (word, box) tuples. 
+        pipeline = keras_ocr.pipeline.Pipeline()
+        prediction_groups = pipeline.recognize([img])
+        #Define the mask for inpainting
+        mask = np.zeros(img.shape[:2], dtype="uint8")
+        rec = 0
+        #If keras-ocr cannot recognize, then only prints "processing image" error and skip image
+        for box in prediction_groups[0]:
+            rec = 1 #in case pipeline not recognize
+            x0, y0 = box[1][0]
+            x1, y1 = box[1][1] 
+            x2, y2 = box[1][2]
+            x3, y3 = box[1][3] 
 
-        x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
-        x_mid1, y_mi1 = midpoint(x0, y0, x3, y3)
+            x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
+            x_mid1, y_mi1 = midpoint(x0, y0, x3, y3)
 
-        #For the line thickness, we will calculate the length of the line between 
-        #the top-left corner and the bottom-left corner.
-        thickness = int(math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ))
+            #For the line thickness, we will calculate the length of the line between 
+            #the top-left corner and the bottom-left corner.
+            thickness = int(math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ))
 
-        #Define the line and inpaint
-        cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mi1), 255,    
-        thickness)
-        inpainted_img = cv2.inpaint(img, mask, 7, cv2.INPAINT_NS)
-    if(rec):
-        return(inpainted_img)
+            #Define the line and inpaint
+            cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mi1), 255,    
+            thickness)
+            inpainted_img = cv2.inpaint(img, mask, 7, cv2.INPAINT_NS)
+        if(rec):
+            return(inpainted_img)
+    except:
+        print(f'Error reading image at: {img_path}')
 
 def process_image(image_path, cleaned_dir):
     # Check if the image has already been processed
