0a03cec2d0c729303e7fe006e593ec07e3f87d1a
Hee Ming Shan <mingshan.hee@gmail.com>
2023-06-01 17:33:44 +0700

    Enabling saving of FLAVA model on FHM dataset

--- models/flava.py
+++ models/flava.py
@@ -7,26 +7,6 @@ import torchmetrics
 
 from transformers import FlavaModel
 
-class MetricCallback(Callback):
-    def __init__(self, output_dict):
-        super().__init__()
-        self.output_dict = output_dict
-        
-    def on_train_start(self, trainer, pl_module):
-        for key, value in self.output_dict.items():
-            setattr(pl_module, f"{key}_train_acc", torchmetrics.Accuracy(task="multiclass", num_classes=value).to('cuda'))
-            setattr(pl_module, f"{key}_train_auroc", torchmetrics.AUROC(task="multiclass", num_classes=value).to('cuda'))
-            
-    def on_validation_start(self, trainer, pl_module):
-        for key, value in self.output_dict.items():
-            setattr(pl_module, f"{key}_val_acc", torchmetrics.Accuracy(task="multiclass", num_classes=value).to('cuda'))
-            setattr(pl_module, f"{key}_val_auroc", torchmetrics.AUROC(task="multiclass", num_classes=value).to('cuda'))
-            
-    def on_test_start(self, trainer, pl_module):
-        for key, value in self.output_dict.items():
-            setattr(pl_module, f"{key}_test_acc", torchmetrics.Accuracy(task="multiclass", num_classes=value).to('cuda'))
-            setattr(pl_module, f"{key}_test_auroc", torchmetrics.AUROC(task="multiclass", num_classes=value).to('cuda'))
-
 
 class FlavaClassificationModel(pl.LightningModule):
     def __init__(
@@ -47,19 +27,22 @@ class FlavaClassificationModel(pl.LightningModule):
         
         # set up metric
         self.cls_dict = cls_dict
-        self.metric_callback = MetricCallback(self.cls_dict)
+        for stage in ["train", "validate", "test"]:
+            for key, value in cls_dict.items():
+                setattr(self, f"{key}_{stage}_acc", torchmetrics.Accuracy(task="multiclass", num_classes=value))
+                setattr(self, f"{key}_{stage}_auroc", torchmetrics.AUROC(task="multiclass", num_classes=value))
        
 
-    def compute_metrics_and_logs(self, preds, labels, loss, prefix, step):
-        acc = getattr(self, f"{prefix}_{step}_acc")
-        auroc = getattr(self, f"{prefix}_{step}_auroc")
+    def compute_metrics_and_logs(self, cls_name, stage, loss, targets, preds):
+        acc = getattr(self, f"{cls_name}_{stage}_acc")
+        auroc = getattr(self, f"{cls_name}_{stage}_auroc")
 
-        acc_value = acc(preds.argmax(dim=-1), labels)
-        auroc_value = auroc(preds, labels)
+        acc_value = acc(preds.argmax(dim=-1), targets)
+        auroc_value = auroc(preds, targets)
 
-        self.log(f'{prefix}_{step}_loss', loss, prog_bar=True)
-        self.log(f'{prefix}_{step}_acc', acc_value, on_step=True, on_epoch=True, sync_dist=True)
-        self.log(f'{prefix}_{step}_auroc', auroc_value, on_step=True, on_epoch=True, sync_dist=True)
+        self.log(f'{cls_name}_{stage}_loss', loss, prog_bar=True)
+        self.log(f'{cls_name}_{stage}_acc', acc_value, on_step=True, on_epoch=True, sync_dist=True)
+        self.log(f'{cls_name}_{stage}_auroc', auroc_value, on_step=True, on_epoch=True, sync_dist=True)
 
 
     def training_step(self, batch, batch_idx):
@@ -70,20 +53,17 @@ class FlavaClassificationModel(pl.LightningModule):
             pixel_values=batch['pixel_values']
         )
 
-        loss = 0
+        total_loss = 0.0
 
-        label_list = []
-        for k,v in self.cls_dict.items():
-            label_list.append(k)
+        for idx, cls_name in enumerate(self.cls_dict.keys()):
+            targets = batch[cls_name]
+            preds = self.mlps[idx](model_outputs.multimodal_embeddings[:, 0])
+            loss = F.cross_entropy(preds, targets)
+            self.compute_metrics_and_logs(cls_name, "train", loss, targets, preds)
 
-        for i in range(len(self.cls_dict)):
-            label_targets = batch[label_list[i]]
-            label_preds = self.mlps[i](model_outputs.multimodal_embeddings[:, 0])
-            label_loss = F.cross_entropy(label_preds, label_targets)
-            loss += label_loss
-            # self.compute_metrics_and_logs(label_preds, label_targets, label_loss,label_list[i] , 'train')
+            total_loss += loss
         
-        return loss
+        return total_loss / len(self.cls_dict)
     
     
     def validation_step(self, batch, batch_idx):
@@ -93,21 +73,17 @@ class FlavaClassificationModel(pl.LightningModule):
             pixel_values=batch['pixel_values']
         )
 
-        loss = 0
+        total_loss = 0.0
 
-        label_list = []
-        for k,v in self.cls_dict.items():
-            label_list.append(k)
+        for idx, cls_name in enumerate(self.cls_dict.keys()):
+            targets = batch[cls_name]
+            preds = self.mlps[idx](model_outputs.multimodal_embeddings[:, 0])
+            loss = F.cross_entropy(preds, targets)
+            self.compute_metrics_and_logs(cls_name, "validate", loss, targets, preds)
 
-        for i in range(len(self.cls_dict)):
-            label_targets = batch[label_list[i]]
-            label_preds = self.mlps[i](model_outputs.multimodal_embeddings[:, 0])
-            label_loss = F.cross_entropy(label_preds, label_targets)
-            loss += label_loss
-            
-            # self.compute_metrics_and_logs(label_preds, label_targets, label_loss,label_list[i] , 'val')
+            total_loss += loss
         
-        return loss
+        return total_loss / len(self.cls_dict)
 
     def predict_step(self, batch, batch_idx):
         model_outputs = self.model(
@@ -116,17 +92,10 @@ class FlavaClassificationModel(pl.LightningModule):
             pixel_values=batch['pixel_values']
         )
 
-        loss = 0
-
-        label_list = []
-        for k,v in self.cls_dict.items():
-            label_list.append(k)
-
         results = {}
-        for i in range(len(self.cls_dict)):
-            label_targets = batch[label_list[i]]
-            label_preds = self.mlps[i](model_outputs.multimodal_embeddings[:, 0])
-            results[label_list[i]] = label_preds
+        for idx, cls_name in enumerate(self.cls_dict.keys()):
+            label_preds = self.mlps[idx](model_outputs.multimodal_embeddings[:, 0])
+            results[cls_name] = label_preds
 
         if "labels" in batch:
             results['labels'] = batch["labels"]
